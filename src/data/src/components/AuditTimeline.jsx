1c6befbb3c65f897a04025bc5290f3ab3a45b99e
Brew-Freedom <Dawn.Brewer@pursuit.org>
2026-02-01 21:13:59 -0500

    Update AuditTimeline.jsx

--- src/data/src/components/AuditTimeline.jsx
+++ src/data/src/components/AuditTimeline.jsx
@@ -1,2 +1,4 @@
+import SimulatedLLMReasoning from "./SimulatedLLMReasoning";
 import ExplainDecision from "./ExplainDecision";
+<SimulatedLLMReasoning reasoning={event.simulatedLLMReasoning} />
 <ExplainDecision explanation={event.aiExplanation} />
