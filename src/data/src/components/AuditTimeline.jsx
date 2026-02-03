e6be773bffd4d1fc350668f2dc12595f14d762b6
Brew-Freedom <Dawn.Brewer@pursuit.org>
2026-02-02 21:34:40 -0500

    Delete src directory

--- src/data/src/components/AuditTimeline.jsx
+++
@@ -1,4 +0,0 @@
-import SimulatedLLMReasoning from "./SimulatedLLMReasoning";
-import ExplainDecision from "./ExplainDecision";
-<SimulatedLLMReasoning reasoning={event.simulatedLLMReasoning} />
-<ExplainDecision explanation={event.aiExplanation} />
